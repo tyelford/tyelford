7c61d561ccf3a7c7c2eec34fe18777f5a584b143
Elford <elfordty@mercy.mercymackay.qld.edu.au>
2016-10-21 13:27:44 +1000

    Starting resume content and styles

---
+++ js/dropSections.js
@@ -0,0 +1,6 @@
+//Javascript to handle the drop out sections on the text overlay screens
+
+function resumeAboutMeClicked(){
+  var content = document.querySelector('#resumeAboutMe p');
+  content.style.display = 'block';
+}
